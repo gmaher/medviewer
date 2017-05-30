876a719e46909e28d14b708d52dff377c1aaf760
gabriel <gmaher2@hotmail.com>
2017-05-30 15:23:27 -0700

    slicer

--- python/fourviewer.py
+++ python/fourviewer.py
@@ -19,7 +19,7 @@ def read_vti(fn):
     reader.SetDataOrigin(0.0, 0.0, 0.0)
     reader.Update()
     return reader
-    
+
 def add_pd(viewer, pd):
     #Create a mapper and actor
     for k in viewer['views'].keys():
@@ -29,6 +29,28 @@ def add_pd(viewer, pd):
         actor.SetMapper(mapper)
         viewer['views'][k].AddActor(actor)
         viewer['views'][k].ResetCamera()
+
+def cut(source):
+    #create a plane to cut,here it cuts in the XZ direction (xz normal=(1,0,0);XY =(0,0,1),YZ =(0,1,0)
+    plane=vtk.vtkPlane()
+    plane.SetOrigin(0,0,0)
+    plane.SetNormal(1,0,0)
+
+    #create cutter
+    cutter=vtk.vtkCutter()
+    cutter.SetCutFunction(plane)
+    cutter.SetInputConnection(source.GetOutputPort())
+    cutter.Update()
+    cutterMapper=vtk.vtkPolyDataMapper()
+    cutterMapper.SetInputConnection(cutter.GetOutputPort())
+
+    #create plane actor
+    planeActor=vtk.vtkActor()
+    planeActor.GetProperty().SetColor(1.0,1,0)
+    planeActor.GetProperty().SetLineWidth(2)
+    planeActor.SetMapper(cutterMapper)
+
+    return planeActor
 
 '''One render window, multiple viewports'''
 rw = vtk.vtkRenderWindow()
@@ -44,18 +66,17 @@ viewer = {}
 viewer['window'] = rw
 viewer['interactor'] = iren
 viewer['views'] = {}
+
+r = read_mha("/home/gabriel/dropbox/vascular_data/OSMSC0101/OSMSC0101-cm.mha")
+
 for i in range(4):
     v = views[i]
     viewer['views'][v] = vtk.vtkRenderer()
     viewer['window'].AddRenderer(viewer['views'][v])
     viewer['views'][v].SetViewport(xmins[i],ymins[i],xmaxs[i],ymaxs[i])
-
-#Create a sphere
-sphereSource = vtk.vtkSphereSource()
-sphereSource.SetCenter(0.0, 0.0, 0.0)
-sphereSource.SetRadius(5)
-
-add_pd(viewer,sphereSource)
+    viewer['views'][v].AddActor(cut(r))
+    viewer['views'][v].ResetCamera()
+#add_pd(viewer,sphereSource)
 
 viewer['window'].Render()
 viewer['window'].SetWindowName('Medview FourView')
--- python/slicer.py
+++ python/slicer.py
@@ -1,9 +1,14 @@
 class Slicer:
-    def __init__(reader):
-        (xMin, xMax, yMin, yMax, zMin, zMax) = reader.GetExecutive().GetWholeExtent(reader.GetOutputInformation(0))
-        (xSpacing, ySpacing, zSpacing) = reader.GetOutput().GetSpacing()
-        (x0, y0, z0) = reader.GetOutput().GetOrigin()
+    def __init__(reader,orientation,min_value=0,max_value=2000):
+        #xmin xmax ymin ymax zmin zmax
+        self.bounds = reader.GetExecutive().GetWholeExtent(reader.GetOutputInformation(0))
 
+        #x y z
+        self.spacing = (xSpacing, ySpacing, zSpacing) = reader.GetOutput().GetSpacing()
+        self.origin  = reader.GetOutput().GetOrigin()
+        xMin,xMax,yMin,yMax,zMin,zMax = self.bounds
+        xspacing,ySpacing,zSpacing = self.spacing
+        x0,y0,z0 = self.origin
         self.center = [x0 + xSpacing * 0.5 * (xMin + xMax),
                   y0 + ySpacing * 0.5 * (yMin + yMax),
                   z0 + zSpacing * 0.5 * (zMin + zMax)]
@@ -27,34 +32,52 @@ class Slicer:
                            0,-1, 0, center[2],
                            0, 0, 0, 1))
 
-        self.oblique = vtk.vtkMatrix4x4()
-        self.oblique.DeepCopy((1, 0, 0, center[0],
-                          0, 0.866025, -0.5, center[1],
-                          0, 0.5, 0.866025, center[2],
-                          0, 0, 0, 1))
-
         # Extract a slice in the desired orientation
-        reslice = vtk.vtkImageReslice()
-        reslice.SetInputConnection(reader.GetOutputPort())
-        reslice.SetOutputDimensionality(2)
-        reslice.SetResliceAxes(sagittal)
-        reslice.SetInterpolationModeToLinear()
+        self.reslice = vtk.vtkImageReslice()
+        self.reslice.SetInputConnection(reader.GetOutputPort())
+        self.reslice.SetOutputDimensionality(2)
+        if orientation=='sagittal':
+            self.reslice.SetResliceAxes(sagittal)
+        elif orientation =='coronal':
+            self.reslice.SetResliceAxes(coronal)
+        elif orientation =='axial':
+            self.reslice.SetResliceAxes(axial)
+
+        self.reslice.SetInterpolationModeToLinear()
 
         # Create a greyscale lookup table
-        table = vtk.vtkLookupTable()
-        table.SetRange(0, 2000) # image intensity range
-        table.SetValueRange(0.0, 1.0) # from black to white
-        table.SetSaturationRange(0.0, 0.0) # no color saturation
-        table.SetRampToLinear()
-        table.Build()
+        self.table = vtk.vtkLookupTable()
+        self.table.SetRange(min_value, max_value) # image intensity range
+        self.table.SetValueRange(0.0, 1.0) # from black to white
+        self.table.SetSaturationRange(0.0, 0.0) # no color saturation
+        self.table.SetRampToLinear()
+        self.table.Build()
 
-        # Map the image through the lookup table
-        color = vtk.vtkImageMapToColors()
-        color.SetLookupTable(table)
-        color.SetInputConnection(reslice.GetOutputPort())
+        # Map the image through the lookup self.table
+        self.color = vtk.vtkImageMapToColors()
+        self.color.SetLookupTable(self.table)
+        self.color.SetInputConnection(self.reslice.GetOutputPort())
 
         # Display the image
-        actor = vtk.vtkImageActor()
-        actor.GetMapper().SetInputConnection(color.GetOutputPort())
+        self.actor = vtk.vtkImageActor()
+        self.actor.GetMapper().SetInputConnection(self.color.GetOutputPort())
 
-    def Update(self):
+def update(obj, interactor, event):
+    if event == "LeftButtonPressEvent":
+        self.slicing = True
+    if event == "LeftButtonReleaseEvent":
+        self.slicing = False
+
+    if self.slicing and event == "MouseMoveEvent":
+        (lastX, lastY) = interactor.GetLastEventPosition()
+        (mouseX, mouseY) = interactor.GetEventPosition()
+
+        deltaY = mouseY - lastY
+        self.reslice.Update()
+        sliceSpacing = self.reslice.GetOutput().GetSpacing()[2]
+        matrix = self.reslice.GetResliceAxes()
+        # move the center point that we are slicing through
+        center = matrix.MultiplyPoint((0, 0, sliceSpacing*deltaY, 1))
+        matrix.SetElement(0, 3, center[0])
+        matrix.SetElement(1, 3, center[1])
+        matrix.SetElement(2, 3, center[2])
